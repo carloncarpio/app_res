8af523e3237316e2c15d61e8929ae7d3f1b08e93
carlo n carpio <carloncarpio@yahoo.com>
2012-09-10 12:20:07 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(:version => 20120909034330) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
-
+end
 ActiveRecord::Schema.define(:version => 20120907081327) do
 
 
