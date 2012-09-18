7cd21fa3895581a038a408f902e0bf9a614f9b97
carlo n carpio <carloncarpio@yahoo.com>
2012-09-18 14:56:05 +0800

    added reservations

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -4,23 +4,23 @@ class CompanyController < ApplicationController
 
   def show_comp
     @user = User.find(params[:id])
-    @vipbbsrsbbs = Vipbbsrsbb.where(:company_name => @user.company_name)
-    @srsaavipaas = Srsaavipaa.where(:company_name => @user.company_name)
-    @srsaas = Srsaa.where(:company_name => @user.company_name)
-    @csaas = Csaa.where(:company_name => @user.company_name)
-    @csas = Csa.where(:company_name => @user.company_name)
-    @srsas = Srsa.where(:company_name => @user.company_name)
-    @vipasrsas = Vipasrsa.where(:company_name => @user.company_name)
-    @srsbvipbs = Srsbvipb.where(:company_name => @user.company_name)
+    @vipbbsrsbbs = Vipbbsrsbb.where(:company_name => @user.company_name).order("ticket_num")
+    @srsaavipaas = Srsaavipaa.where(:company_name => @user.company_name).order("ticket_num")
+    @srsaas = Srsaa.where(:company_name => @user.company_name).order("ticket_num")
+    @csaas = Csaa.where(:company_name => @user.company_name).order("ticket_num")
+    @csas = Csa.where(:company_name => @user.company_name).order("ticket_num")
+    @srsas = Srsa.where(:company_name => @user.company_name).order("ticket_num")
+    @vipasrsas = Vipasrsa.where(:company_name => @user.company_name).order("ticket_num")
+    @srsbvipbs = Srsbvipb.where(:company_name => @user.company_name).order("ticket_num")
 
-    @lbdds = Lbdd.where(:company_name => @user.company_name)
-    @lbccs = Lbcc.where(:company_name => @user.company_name)
-    @lbbbs = Lbbb.where(:company_name => @user.company_name)
-    @lbaas = Lbaa.where(:company_name => @user.company_name)
-    @lbas = Lba.where(:company_name => @user.company_name)
-    @lbbs = Lbb.where(:company_name => @user.company_name)
-    @lbcs = Lbc.where(:company_name => @user.company_name)
-    @lbds = Lbd.where(:company_name => @user.company_name)
+    @lbdds = Lbdd.where(:company_name => @user.company_name).order("ticket_num")
+    @lbccs = Lbcc.where(:company_name => @user.company_name).order("ticket_num")
+    @lbbbs = Lbbb.where(:company_name => @user.company_name).order("ticket_num")
+    @lbaas = Lbaa.where(:company_name => @user.company_name).order("ticket_num")
+    @lbas = Lba.where(:company_name => @user.company_name).order("ticket_num")
+    @lbbs = Lbb.where(:company_name => @user.company_name).order("ticket_num")
+    @lbcs = Lbc.where(:company_name => @user.company_name).order("ticket_num")
+    @lbds = Lbd.where(:company_name => @user.company_name).order("ticket_num")
     @patrons = Patron.where(:company_name => @user.company_name).order("ticket_num")
 
     @all = @vipbbsrsbbs.size + @srsaavipaas.size + @srsaas.size + @csaas.size + @csas.size + @srsas.size + @vipasrsas.size + @srsbvipbs.size + @lbdds.size + @lbccs.size + @lbbbs.size + @lbaas.size + @lbas.size + @lbbs.size + @lbcs.size + @lbds.size + @patrons.size
@@ -37,6 +37,19 @@ class CompanyController < ApplicationController
    	#@vipbbsrsbb = Vipbbsrsbb.find(params[:company_email])
     
     UserMailer.send_mail(@user.company_email,@user.company_name).deliver
+    #@email = Vipbbsrsbb.all
+    #@comp = Vipbbsrsbb.all
+    #UserMailer.send_reserves(@email,@comp).deliver
+    redirect_to(:controller => 'access',:action => 'public')
+  end
+#jing.almaden@peoplesparxph.com
+
+  def submit_res
+    @user = User.find(params[:id])
+    #@user = User.where(:id => params[:id])
+    #@vipbbsrsbb = Vipbbsrsbb.find(params[:company_email])
+    
+    UserMailer.mysubmit(@user.company_email,@user.company_name).deliver
     #@email = Vipbbsrsbb.all
     #@comp = Vipbbsrsbb.all
     #UserMailer.send_reserves(@email,@comp).deliver
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -54,4 +54,38 @@ class UserMailer < ActionMailer::Base
     mail(:to => @company_email, :subject => 'Test')
   end
 
+  def mysubmit(email,company_name)
+    @company_email = email
+    @company_name = company_name
+
+
+    @vipbbsrsbbs = Vipbbsrsbb.where(:company_name => @company_name)
+    @srsaavipaas = Srsaavipaa.where(:company_name => @company_name)
+    @vipbbsrsbb = Vipbbsrsbb.where(:company_name => company_name)
+    @srsaavipaas = Srsaavipaa.where(:company_name => company_name)
+    @srsaas = Srsaa.where(:company_name => company_name)
+    @csaas = Csaa.where(:company_name => company_name)
+    @csas = Csa.where(:company_name => company_name)
+    @srsas = Srsa.where(:company_name => company_name)
+    @vipasrsas = Vipasrsa.where(:company_name => company_name)
+    @srsbvipbs = Srsbvipb.where(:company_name => company_name)
+    @patrons = Patron.where(:company_name => company_name)
+
+    @lbdds = Lbdd.where(:company_name => company_name)
+    @lbccs = Lbcc.where(:company_name => company_name)
+    @lbbbs = Lbbb.where(:company_name => company_name)
+    @lbaas = Lbaa.where(:company_name => company_name)
+    @lbas = Lba.where(:company_name => company_name)
+    @lbbs = Lbb.where(:company_name => company_name)
+    @lbcs = Lbc.where(:company_name => company_name)
+    @lbds = Lbd.where(:company_name => company_name)
+
+    #@all = @vipbbsrsbb.size + @srsaavipaas.size + @srsaas.size + @csaas.size + @csas.size + @srsas.size + @vipasrsas.size + @srsbvipbs.size + @lbdds.size + @lbccs.size + @lbbbs.size + @lbaas.size + @lbas.size + @lbbs.size + @lbcs.size + @lbds.size
+    @all = @vipbbsrsbbs.size + @srsaavipaas.size + @srsaas.size + @csaas.size + @csas.size + @srsas.size + @vipasrsas.size + @srsbvipbs.size + @lbdds.size + @lbccs.size + @lbbbs.size + @lbaas.size + @lbas.size + @lbbs.size + @lbcs.size + @lbds.size + @patrons.size
+
+    mail(:to => 'hrccicons2012@peoplesparxph.com',
+        :cc => 'jing.almaden@peoplesparxph.com',
+        :bcc => "carlo.carpio@capstone-ph.com", :subject => 'Reserve Seats')
+  end
+
 end
