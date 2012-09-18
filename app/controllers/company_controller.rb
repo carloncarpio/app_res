a485effee2298809f5d976bec1da7e7d8c4b5d5a
carlo n carpio <carloncarpio@yahoo.com>
2012-09-18 09:20:12 +0800

    edited the viw

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -21,7 +21,7 @@ class CompanyController < ApplicationController
     @lbbs = Lbb.where(:company_name => @user.company_name)
     @lbcs = Lbc.where(:company_name => @user.company_name)
     @lbds = Lbd.where(:company_name => @user.company_name)
-    @patrons = Patron.where(:company_name => @user.company_name).order("id ASC")
+    @patrons = Patron.where(:company_name => @user.company_name).order("ticket_num")
 
     @all = @vipbbsrsbbs.size + @srsaavipaas.size + @srsaas.size + @csaas.size + @csas.size + @srsas.size + @vipasrsas.size + @srsbvipbs.size + @lbdds.size + @lbccs.size + @lbbbs.size + @lbaas.size + @lbas.size + @lbbs.size + @lbcs.size + @lbds.size + @patrons.size
 
